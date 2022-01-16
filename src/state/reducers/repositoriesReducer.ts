e0bc8ba54b2b08607c0e59073e720b0166a6fdd7
Matthew Henry <tadaemusic@gmail.com>
2022-01-16 12:59:40 -0500

    Create Main Reducer

---
+++ src/state/reducers/repositoriesReducer.ts
@@ -0,0 +1,50 @@
+interface RepositoriesState {
+  loading: boolean;
+  error: string | null;
+  data: string[];
+}
+// payload? = may or may not have a payload and if it does it will be any type
+
+interface SearcHrepositoriesAction {
+  type: ActionType.SEARCH_REPOSITORIES;
+}
+
+interface SearchRepositoriesSuccessAction {
+  type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
+  payload: string[];
+}
+interface SearchRepositoriesErrorAction {
+  type: ActionType.SEARCH_REPOSITORIES_ERROR;
+  payload: string;
+}
+
+type Action =
+  | SearcHrepositoriesAction
+  | SearchRepositoriesSuccessAction
+  | SearchRepositoriesErrorAction;
+
+// enum = essentially an object
+enum ActionType {
+  SEARCH_REPOSITORIES = "search_repositories",
+  SEARCH_REPOSITORIES_SUCCESS = "search_repositories_success",
+  SEARCH_REPOSITORIES_ERROR = "search_repositories_error",
+}
+
+const reducer = (
+  state: RepositoriesState,
+  action: Action
+): RepositoriesState => {
+  switch (action.type) {
+    case ActionType.SEARCH_REPOSITORIES:
+      return { loading: true, error: null, data: [] };
+    case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+      // certain that 'action' is SearchRepositoriesSuccessAction
+      return { loading: false, error: null, data: action.payload };
+    case ActionType.SEARCH_REPOSITORIES_ERROR:
+      return { loading: false, error: action.payload, data: [] };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
